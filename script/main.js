86f5ec8fb66980f391d1a2d0b98d3bf328872681
smichelle05a <smichelle05a@gmail.com>
2021-04-30 16:41:46 -0300

    upd animaciones navegación y ejercicio obligatorio css

--- script/main.js
+++ script/main.js
@@ -10,14 +10,59 @@ let section = () => {
 }
 let activeSection = document.querySelector(section());
 
+const animateIn = (element, animation, prefix = 'animate__') =>
+    // We create a Promise and return it
+    new Promise((resolve, reject) => {
+        const animationName = `${prefix}${animation}`;
+        const node = element;
+
+        node.classList.remove('d-none');
+        node.classList.add(animationName);
+
+        // When the animation ends, we clean the classes and resolve the Promise
+        function handleAnimationEnd(event) {
+            event.stopPropagation();
+            node.classList.remove(animationName, 'd-none');
+            resolve('Animation ended');
+        }
+
+        node.addEventListener('animationend', handleAnimationEnd, { once: true });
+    });
+
+const animateOut = (element, animation, prefix = 'animate__') =>
+    // We create a Promise and return it
+    new Promise((resolve, reject) => {
+        const animationName = `${prefix}${animation}`;
+        const node = element;
+
+        node.classList.add(animationName);
+
+        // When the animation ends, we clean the classes and resolve the Promise
+        function handleAnimationEnd(event) {
+            event.stopPropagation();
+            node.classList.add('d-none');
+            node.classList.remove(animationName);
+            resolve('Animation ended');
+        }
+
+        node.addEventListener('animationend', handleAnimationEnd, { once: true });
+    });
 
 let sectionSelect = () => {
-    activeSection.classList.remove('d-none');
+    animateIn(activeSection, 'fadeIn');
     for (let i of nav) {
         i.addEventListener('click', (e) => {
-            activeSection.classList.add('d-none');
-            activeSection = document.querySelector(e.target.hash);
-            activeSection.classList.remove('d-none');
+            animateOut(activeSection, 'fadeOut');
+            console.log(activeSection);
+            if (document.querySelector(e.target.hash) == null) {
+                activeSection = document.querySelector('#home')
+            } else {
+                activeSection = document.querySelector(e.target.hash);
+            }
+            console.log(activeSection)
+            setTimeout(() => {
+                animateIn(activeSection, 'fadeIn');
+            }, 1000);
         });
     }
 }
